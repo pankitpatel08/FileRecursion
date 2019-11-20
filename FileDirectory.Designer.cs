711e80d9193d8d30601ace9b92444f5ceb74508b
pankitpatel08 <umapatel.er@gmail.com>
2019-11-20 17:58:06 +1300

    File Recursion Code

---
+++ FileDirectory.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace FileRecursion
+{
+    partial class FileDirectory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.ItemHeight = 16;
+            this.lstFiles.Location = new System.Drawing.Point(33, 67);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(603, 244);
+            this.lstFiles.TabIndex = 0;
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(33, 16);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(308, 22);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(347, 11);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(81, 34);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(33, 328);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 46);
+            this.btnRename.TabIndex = 3;
+            this.btnRename.Text = "Rename File";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.BtnRename_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(168, 328);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 46);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete File";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(297, 328);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(75, 46);
+            this.btnModify.TabIndex = 5;
+            this.btnModify.Text = "Modify File";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.BtnModify_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(432, 328);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 46);
+            this.btnCopy.TabIndex = 6;
+            this.btnCopy.Text = "Clone File";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.BtnCopy_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(561, 328);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 46);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // FileDirectory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 391);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.lstFiles);
+            this.Name = "FileDirectory";
+            this.Text = "File Recursion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ FileDirectory.cs
@@ -0,0 +1,146 @@
+﻿using RecursionSolver;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FileRecursion
+{
+    public partial class FileDirectory : Form
+    {
+        OrbitoryOperation objOrbOpe = new OrbitoryOperation();
+
+        #region Constructor
+        public FileDirectory()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// Find File from folders recursion function
+        /// </summary>
+        /// <param name="path"></param>
+        private void FindFiles(string path)
+        {
+            var files = Directory.GetFiles(path).Where(s => s.EndsWith(".doc") || s.EndsWith(".docx") || s.EndsWith(".txt"));
+            foreach (string fileName in files)
+            {
+                lstFiles.Items.Add(fileName);
+            }
+            foreach (string directory in Directory.GetDirectories(path))
+            {
+                FindFiles(directory);
+            }
+        }
+
+        /// <summary>
+        /// Get Selected file from ListBox
+        /// </summary>
+        /// <returns></returns>
+        private string GetSelectedFile()
+        {
+            return lstFiles.Items[lstFiles.SelectedIndex].ToString();
+        }
+
+        /// <summary>
+        /// Reload file after operation
+        /// </summary>
+        private void ReloadFiles()
+        {
+            lstFiles.Items.Clear();
+            FindFiles(txtFilePath.Text);
+        }
+        #endregion
+
+        #region Private form Events
+        /// <summary>
+        /// Browse Button Click Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            using (var fldrDlg = new FolderBrowserDialog())
+            {
+                if (fldrDlg.ShowDialog() == DialogResult.OK)
+                {
+                    txtFilePath.Text = fldrDlg.SelectedPath;
+                    lstFiles.Items.Clear();
+                    FindFiles(txtFilePath.Text);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Button Rename Click Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnRename_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string selectedItem = GetSelectedFile();
+                objOrbOpe.RenameFile(selectedItem);
+                MessageBox.Show("File renamed!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ReloadFiles();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Rename File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Button Delete Click Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            string selectedItem = GetSelectedFile();
+            objOrbOpe.DeleteFile(selectedItem);
+            MessageBox.Show("File deleted!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ReloadFiles();
+        }
+
+        /// <summary>
+        /// Button Modify Click Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnModify_Click(object sender, EventArgs e)
+        {
+            string selectedItem = GetSelectedFile();
+            objOrbOpe.ModifyFile(selectedItem);
+            MessageBox.Show("File modified!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ReloadFiles();
+        }
+
+        /// <summary>
+        /// Button Copy Click Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnCopy_Click(object sender, EventArgs e)
+        {
+            string selectedItem = GetSelectedFile();
+            objOrbOpe.CloneFile(selectedItem);
+            MessageBox.Show("File copied!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ReloadFiles();
+        }
+
+        /// <summary>
+        /// Application Exit
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        #endregion
+    }
+}
